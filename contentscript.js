6a084b9f85f2a6e038c046c99a5a727956d47b5a
Benjamin Grosse <ste3ls@gmail.com>
2013-01-02 20:24:22 +0100

    also show link-only seasons

--- contentscript.js
+++ contentscript.js
@@ -2,20 +2,28 @@ chrome.extension.onMessage.addListener(function(command, sender, sendResponse) {
 	console.log('message.', command);
 	if (command === 'seasons') {
 		var elements = document.getElementsByClassName('subscription');
-		if (!elements || elements.length === 0) {
-			sendResponse(null);
-			
-		} else {
-			console.log('seasons: ', elements);
-			var seasons = Array.prototype.slice.call(elements).map(function(a) {
-				return a.getAttribute('temporada');
-			}).reduce(function(list, element) {
-				if (element) {
-					list.push(element);
-				}
-				return list;
-			}, []);
-			sendResponse(seasons.join(','));
-		}
+		
+		var seasons = Array.prototype.slice.call(elements).map(function(a) {
+			return a.getAttribute('temporada');
+		}).reduce(function(list, element) {
+			if (element) {
+				list.push(element);
+			}
+			return list;
+		}, []);
+		
+		var elements = document.getElementsByClassName('transaction');
+		seasons = Array.prototype.slice.call(elements).map(function(a) {
+			return a.getAttribute('temporada');
+		}).reduce(function(list, element) {
+			if (element) {
+				list.push(element);
+			}
+			return list;
+		}, seasons);
+		
+		seasons.sort();
+		
+		sendResponse(seasons.join(','));
     }
 });
